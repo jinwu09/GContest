2937ed25d5d01f73d19bd331f7bd3a502baca3cd
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2023-03-07 15:07:56 +0800

    stable for now

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,46 @@
 import express, { Express, Request, Response } from "express";
-import { users } from "./routes/users/userIndex";
+import { usersRouter } from "./routes/users/userIndex";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+import * as quizindex from "./routes/quiz/quizindex";
+import { Socket } from "dgram";
 
 const app: Express = express();
-const port = 8080;
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    credentials: true,
+    allowedHeaders: ["my-custom-header"],
+  },
+});
 
+const port = 8080;
 app.use(express.json());
 
+// test socket
+io.on("connection", (socket) => {
+  app.set("socketio", socket);
+  console.log(socket.id + " user connected");
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+});
+
+// Socket route
+io.on("connection", quizindex.socketlistener);
+
 // routes
-app.use("/users", users);
+app.use("/user", usersRouter);
+app.use("/quiz", quizindex.QuizRouter);
 
 app.get("/test", (req: Request, res: Response) => {
   res.send("the typescript express get method is working");
 });
 
-app.listen(port, () => {
-  console.log("The server is running in port 8080");
+httpServer.listen(port, () => {
+  console.log("The server is running in port " + port);
 });
--- backend/src/routes/users/userIndex.ts
+++ backend/src/routes/users/userIndex.ts
@@ -1,7 +1,48 @@
 import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { Code, sendTemplate } from "../../methods/template";
+export const usersRouter: Router = Router();
 
-export const users: Router = Router();
+const prisma = new PrismaClient();
 
-users.get("/test", async (req: Request, res: Response) => {
+usersRouter.get("/test", async (req: Request, res: Response) => {
   res.send("testing users/test");
+});
+
+usersRouter.post("/register", async (req: Request, res: Response) => {
+  interface user {
+    first_name: string;
+    last_name: string;
+    username: string;
+    email: string;
+    password: string;
+  }
+  const data: user = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const createUser = await prisma.users
+    .create({
+      data: {
+        first_name: data.first_name,
+        last_name: data.last_name,
+        username: data.username,
+        school: req.body.school == null ? req.body.school : null,
+        email: data.email,
+        password: data.password,
+      },
+    })
+    .catch((e) => {
+      console.log("from create user");
+      console.log(e);
+      res
+        .status(Code.S400_Bad_Request)
+        .send(sendTemplate("missing credential"));
+    })
+    .then(() => {
+      res.send(sendTemplate("successfully created user"));
+    });
 });
