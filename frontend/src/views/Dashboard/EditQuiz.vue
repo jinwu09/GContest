49f32c55f52c4026f9e05c00994cd4d448e18f9a
ravjanskie <jan2skie@gmail.com>
2023-03-20 18:45:00 +0800

    update edit quiz

--- frontend/src/views/Dashboard/EditQuiz.vue
+++ frontend/src/views/Dashboard/EditQuiz.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue'
+import Vue from "vue";
+
 </script>
 
 <template>
@@ -17,7 +19,17 @@ import NavBar from '@/components/NavBar.vue'
             <!-- second main column first row -->
             <div class="row my-3">
                 <div class="col-3">
-                    <div class="w-100 btn btn-success">Point Dropdown</div>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle w-100" data-bs-toggle="dropdown" aria-expanded="false">
+                            Point Dropdown
+                        </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Passed</a></li>
+                        <li><a class="dropdown-item" href="#">Failed</a></li>
+                        <li><a class="dropdown-item" href="#">Unfinished</a></li>
+                        <li><a class="dropdown-item" href="#">Finished</a></li>
+                    </ul>
+                    </div>
                 </div>
                 <div class="col-3">
                     <div class="w-100"></div>
@@ -26,7 +38,7 @@ import NavBar from '@/components/NavBar.vue'
                     <div class="w-100 btn btn-danger">Cancel</div>
                 </div>
                 <div class="col-3">
-                    <div class="w-100 btn btn-secondary">Save</div>
+                    <div class="w-100 btn btn-success">Save</div>
                 </div>
             </div>
 
@@ -40,19 +52,31 @@ import NavBar from '@/components/NavBar.vue'
             <!-- second main column third row -->
             <div class="col-12 d-flex">
                 <div class="w-25 p-2 questionBox rounded m-2">
-                    <div class="btn btn-danger w-100 bt1 mb-3">Wrong</div>
+                    <label class="switch w-100 mb-3">
+                        <input type="checkbox">
+                        <span class="slider rounded"></span>
+                    </label>    
                     <input class="noBorder questionBoxColor w-100 pb-4" placeholder="Edit Answer"/>
                 </div>
                 <div class=" w-25 p-2 questionBox rounded m-2">
-                    <div class="btn btn-danger w-100 bt1 mb-3">Wrong</div>
+                    <label class="switch w-100 mb-3">
+                        <input type="checkbox">
+                        <span class="slider rounded"></span>
+                    </label>    
                     <input class="noBorder questionBoxColor w-100" placeholder="Edit Answer"/>
                 </div>
                 <div class="w-25 p-2 questionBox rounded m-2">
-                    <div class="btn btn-danger w-100 bt1 mb-3">Wrong</div>
+                    <label class="switch w-100 mb-3">
+                        <input type="checkbox">
+                        <span class="slider rounded"></span>
+                    </label>    
                     <input class="noBorder questionBoxColor w-100" placeholder="Edit Answer"/>
                 </div>
                 <div class="w-25 p-2 questionBox rounded m-2">
-                    <div class="btn btn-danger w-100 bt1 mb-3">Wrong</div>
+                    <label class="switch w-100 mb-3">
+                        <input type="checkbox">
+                        <span class="slider rounded"></span>
+                    </label>    
                     <input class="noBorder questionBoxColor w-100" placeholder="Edit Answer"/>
                 </div>
             </div>
@@ -92,5 +116,51 @@ import NavBar from '@/components/NavBar.vue'
     .bt1{
         padding: 10px !important;
         margin: 10x !important;
+    }
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+    }
+
+/* Hide default HTML checkbox */
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+/* The slider */
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: red;
+        -webkit-transition: .4s;
+        transition: .4s;
+    }
+    .slider:before {
+        position: absolute;
+        color: white;
+        height: 26px;
+        width: 100px;
+        left: 80px;
+        bottom: 4px;
+        background-color: None;
+        -webkit-transition: .4s;
+        transition: .4s;
+    }
+    input:checked + .slider {
+        content: "Correct";
+        background-color: green !important;
+    }
+    input:checked + .slider:before {
+        -webkit-transform: translateX(26px);
+        -ms-transform: translateX(26px);
+        transform: translateX(26px);
     }
 </style>
