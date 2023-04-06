542958298245c7548b7f42cd9baff57a1e39b84f
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2023-04-06 14:27:27 +0800

    unstable function

--- backend/src/routes/quiz/methods/AnswerMethods.ts
+++ backend/src/routes/quiz/methods/AnswerMethods.ts
@@ -2,13 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const Answer = {
-  Create: async (quizID: string, questionID: string, choiceID: string) => {
-    const createAnswer = await prisma.answer.create;
-  },
-  Generate: async (userID: string, quizID: string) => {
-    const generateAnswer = await prisma.quiz.createMany({
-      data: {},
-    });
-  },
-};
+// export const Answer = {
+//   Create: async (quizID: string, questionID: string, choiceID: string) => {
+//     const createAnswer = await prisma.answer.create;
+//   },
+//   Generate: async (userID: string, quizID: string) => {
+//     const generateAnswer = await prisma.quiz.createMany({
+//       data: {},
+//     });
+//   },
+// };
