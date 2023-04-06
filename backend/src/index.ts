c9e0c2bc540f852f0f69a6c95f9416204b8198df
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2023-04-05 19:25:05 -0700

    Revert "Created new branch for cleaning up backend files"

--- backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { QuizRouter } from "./routes/quiz";
-// const QuizRouter = require('./routes/quiz')
-const router = Router()
-
-router.use('/quiz', QuizRouter)
-
-export const indexRouter: Router = router
