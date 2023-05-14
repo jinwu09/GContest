2f96b357d49e78659df66f71423ee114e7e64ed2
Markdiocre <202010597@gordoncollege.edu.ph>
2023-05-14 22:56:48 +0800

    Experiment on socket io speed

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,7 +20,7 @@ import axios from 'axios'
 import './assets/main.css'
 
 // axios.defaults.baseURL = 'http://127.0.0.1:8080/api'
-// axios.defaults.baseURL = 'http://localhost:8080/api'
+// axios.defaults.baseURL = 'http://192.168.18.185:8080/api'
 // axios.defaults.withCredentials = true;
 axios.defaults.baseURL = (import.meta.env.VITE_APP_STATUS == 'LOCAL'? import.meta.env.VITE_API_LOCAL_URL:import.meta.env.VITE_API_LOCAL_URL) + 'api/'
 
