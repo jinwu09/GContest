53e866c3aae44d9607d803e3b784f127d4ce105f
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2023-05-16 19:13:26 +0800

    history update

--- backend/app.ts
+++ backend/app.ts
@@ -8,12 +8,14 @@ import { indexRouter } from "./src";
 import { SocketListener } from "./src/services/socket/SocketIndex";
 import { SocketCheckToken } from "./src/middlewares/Socket/SocketCheckToken";
 import { TokenToUserID } from "./src/middlewares/Socket/TokenToUserID";
+import { Code, sendTemplate } from "./src/methods/template";
+import { send } from "process";
 
 export const app: Express = express();
 const httpServer = createServer(app);
 export const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:8080","http://192.168.137.1:5173/","*"],
+    origin: ["http://localhost:8080", "http://192.168.137.1:5173/", "*"],
     // origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
@@ -30,6 +32,12 @@ app.use("/api", indexRouter);
 
 app.get("/test", (req: Request, res: Response) => {
   res.send("the typescript express get method is working");
+});
+
+app.all("/*", (req: Request, res: Response) => {
+  res
+    .status(Code.S400_Bad_Request)
+    .send(sendTemplate({ msg: "Route doesn't exist" }, Code.S400_Bad_Request));
 });
 
 httpServer.listen(port, () => {
--- backend/src/routes/history.ts
+++ backend/src/routes/history.ts
@@ -1,17 +1,19 @@
 import { Router, Request, Response } from "express";
 import { body } from "express-validator/src/middlewares/validation-chain-builders";
 import { PrismaClient } from "@prisma/client";
-import { validationResult } from "express-validator";
+import { param, validationResult } from "express-validator";
 import { sendTemplate, Code } from "../methods/template";
-import { group } from "console";
-import { send } from "process";
+import { Validate } from "../middlewares/validator";
 
 const prisma = new PrismaClient();
 const router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
+router.get("/joined", async (req: Request, res: Response) => {
   const groupSession = await prisma.answer.groupBy({
     by: ["quizSessionId"],
+    orderBy: {
+      quizSessionId: "desc",
+    },
   });
   let getUserHistory: any[] = [];
 
@@ -68,65 +70,137 @@ router.get("/", async (req: Request, res: Response) => {
   console.log(getUserHistory);
   res.status(Code.s200_OK).send(sendTemplate(getUserHistory, Code.s200_OK));
 });
-router.get("/:quizID", async (req: Request, res: Response) => {
-  const groupSession = await prisma.answer.groupBy({
-    by: ["quizSessionId"],
-    where: {
-      quizId: Number(req.params.quizID),
-    },
-  });
-  // let getQuizHistory: any[] = [];
+router.get(
+  "/joined/:quizID",
+  param("quizID").isNumeric(),
+  Validate,
+  async (req: Request, res: Response) => {
+    const groupSession = await prisma.answer.groupBy({
+      by: ["quizSessionId"],
+      where: {
+        usersId: res.locals.userId,
+        quizId: Number(req.params.quizID),
+      },
+    });
+    let getUserHistory: any[] = [];
 
-  // for (const item of groupSession) {
-  //   const QuizSession = await prisma.quiz
-  //     .findFirst({
-  //       where: {
-  //         answer: {
-  //           some: {
-  //             quizSessionId: item.quizSessionId,
-  //           },
-  //         },
-  //       },
-  //       select: {
-  //         id: true,
-  //         title: true,
-  //         question: {
-  //           select: {
-  //             score: true,
-  //           },
-  //         },
-  //         answer: {
-  //           where: {
-  //             quizSessionId: item.quizSessionId,
-  //             choice: {
-  //               is_correct: true,
-  //             },
-  //           },
-  //           select: {
-  //             Question: {
-  //               select: {
-  //                 score: true,
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //     })
-  //     .then(async (data) => {
-  //       console.log(data);
-  //       let QuizScore: number = 0;
-  //       let UserScore: number = 0;
-  //       data?.question.forEach((item) => {
-  //         QuizScore += item.score;
-  //       });
-  //       data?.answer.forEach((item) => {
-  //         UserScore += item.Question.score;
-  //       });
-  //       return { id: data?.id, title: data?.title, QuizScore, UserScore };
-  //     });
-  //   getQuizHistory.push(QuizSession);
-  // }
-  res.status(Code.s200_OK).send(sendTemplate(groupSession, Code.s200_OK));
-});
+    for (const item of groupSession) {
+      const QuizSession = await prisma.quiz
+        .findFirst({
+          where: {
+            answer: {
+              some: {
+                quizSessionId: item.quizSessionId,
+              },
+            },
+          },
+          select: {
+            id: true,
+            title: true,
+            question: {
+              select: {
+                id: true,
+                content: true,
+                score: true,
+                choice: true,
+                answer: true,
+              },
+            },
+          },
+        })
+        .then(async (data) => {
+          console.log(data);
+          return data;
+        });
+      getUserHistory.push(QuizSession);
+    }
+    res.status(Code.s200_OK).send(sendTemplate(getUserHistory, Code.s200_OK));
+  }
+);
+// router.get()
+router.get(
+  "/:quizID",
+  param("quizID").isNumeric(),
+  Validate,
+  async (req: Request, res: Response) => {
+    const groupSession = await prisma.answer.groupBy({
+      by: ["quizSessionId"],
+      where: {
+        quizId: Number(req.params.quizID),
+      },
+      orderBy: {
+        quizSessionId: "desc",
+      },
+    });
+    console.log(groupSession);
+    let getQuizHistory: any[] = [];
+
+    for (const item of groupSession) {
+      const QuizSession = await prisma.user
+        .findMany({
+          where: {
+            Answer: {
+              some: {
+                quizSessionId: Number(item.quizSessionId),
+              },
+            },
+          },
+          select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            Answer: {
+              where: {
+                quizSessionId: Number(item.quizSessionId),
+                choice: {
+                  is_correct: true,
+                },
+              },
+              select: {
+                Question: {
+                  select: {
+                    score: true,
+                  },
+                },
+                choice: {
+                  select: {
+                    is_correct: true,
+                  },
+                },
+              },
+            },
+          },
+        })
+        .then((data) => {
+          console.log(data);
+          return data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      let getScoreTotal: any = [];
+      QuizSession?.forEach((item) => {
+        let obj = {
+          id: item.id,
+          first_name: item.first_name,
+          last_name: item.last_name,
+          score: 0,
+        };
+        item.Answer.forEach((item) => {
+          if (item.choice?.is_correct == true) {
+            obj.score += item.Question?.score;
+          }
+        });
+        getScoreTotal.push(obj);
+      });
+      const data = {
+        Session: item.quizSessionId,
+        totalScore: getScoreTotal,
+      };
+      getQuizHistory.push({ data });
+    }
+    res.status(Code.s200_OK).send(sendTemplate(getQuizHistory, Code.s200_OK));
+  }
+);
 
 export const HistoryRouter: Router = router;
